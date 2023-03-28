3c0dfa3cfc495a46d93474a7da55fed01d3f95f4
mohini <Mohiniruhani112@gmail.com>
2023-03-28 17:40:08 -0400

    updated to string input

--- src/calculator.java
+++ src/calculator.java
@@ -3,31 +3,37 @@
 import java.util.Scanner;
 public class calculator {
     public static void main(String args[])
-    { int a,b,ch;
+    { int a,b;
+        String ch;
         System.out.print("Enter the first number:");
         Scanner src=new Scanner(System.in);
         a=src.nextInt();
         System.out.print("Enter the second number:");
         b=src.nextInt();
+        src.nextLine();
+
         System.out.print("Enter your operation choice:");
-        ch=src.nextInt();
-        if(ch==1)
+
+        ch=src.nextLine();
+
+        if(ch=="ADD")
+
         {
             System.out.print("Addition of numbers is ;"+(a+b));
         }
-        else if(ch==2)
+        else if(ch=="SUB")
         {
             System.out.print("Subtraction of numbers is ;"+(a-b));
         }
-        else if(ch==3)
+        else if(ch=="MUL")
         {
             System.out.print("Multiplication of numbers is ;"+(a*b));
         }
-        else if(ch==4)
+        else if(ch=="PERCENT")
         {
             System.out.print("Percentage of number is ;"+((a/a+b)*100));
         }
-        else if(ch==4)
+        else if(ch=="DIV")
         {
             System.out.print("Division of numbers is ;"+(a/b));
         }
