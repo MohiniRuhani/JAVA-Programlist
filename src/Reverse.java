908e5bc4e0df6608baa8e439debe33fabf56fa5f
mohini <Mohiniruhani112@gmail.com>
2023-03-31 10:28:58 -0400

    reverse of two varibales by using third variable

--- src/Reverse.java
+++ src/Reverse.java
@@ -1,4 +1,4 @@
-// Reverse of string with using third variable
+// Reverse of two numbers with using third variable
 
 import java. util.Scanner;
 public class Reverse {
@@ -10,12 +10,13 @@ public class Reverse {
         a=src.nextInt();
         System.out.print("enter the second value :");
         b=src.nextInt();
+        System.out.println("value of first and second  elements before swapping is  :"+ a+" "+b);
         temp=a;
         a=b;
         b=temp;
 
-        System.out.println("value of first element after swapping is  :"+ a);
-        System.out.print("value of second element after swapping is  :"+ b);
+
+        System.out.println("value of first and second element after swapping is  :"+ a+" "+b);
 
     }
 }
