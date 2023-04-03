00a655c7c9d755ae4bfd21a0928b9b0dbf0380da
mohini <Mohiniruhani112@gmail.com>
2023-04-03 14:59:10 -0400

    reverse of a number

--- src/ReverseNumber.java
+++ src/ReverseNumber.java
@@ -11,9 +11,9 @@ public class ReverseNumber {
         n=src.nextInt(); // n=100
         while(n>0)      // case1:100>0, yes  !! case2: 10>0,yes !! case3: 1>0,yes
         {
-            r=n%10;     // r=0               !! r=10%10=0        !! r=1/%10=
-            System.out.print(r); // 0        !! 0
-            n=n/10;            // n=100/10=10 !!n=10/10=1
+            r=n%10;     // r=0               !! r=10%10=0        !! r=1/%10=1
+            System.out.print(r); // 0        !!    0              !! 1
+            n=n/10;            // n=100/10=10 !!n=10/10=1         !! n=1/10=0.1 but we consider only 1 because of int
         }
     }
 
